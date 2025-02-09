7b450082a8b779c93b1e535365238d03e84fffe1
zackntumba <zackntumba@outlook.com>
2025-02-10 00:45:36 +0100

    Add files via upload

---
+++ METEO APP/script.js
@@ -0,0 +1,54 @@
+const apikey = "6eb19228df8c9f89c93d56184a280a54";
+const apiurl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkweather(city) {
+  const response = await fetch(apiurl + city + `&appid=${apikey}`);
+
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+    document.querySelector(".info").style.display = "none";
+    document.querySelector(".sand").style.display = "none";
+  } else {
+    let data = await response.json();
+
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "°c";
+    // document.querySelector(".temp").innerHTML = data.main.temp + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    document.querySelector(".feels").innerHTML =
+      "t° ressentie : " + data.main.feels_like + "°c";
+    document.querySelector(".country").innerHTML = data.sys.country;
+
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "images/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "images/clear.png";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "images/rain.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "images/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.src = "images/mist.png";
+    } else if (data.weather[0].main == "Snow") {
+      weatherIcon.src = "images/snow.png";
+    }
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+    document.querySelector(".info").style.display = "block";
+    document.querySelector(".sand").style.display = "block";
+  }
+}
+
+searchBtn.addEventListener("click", () => {
+  checkweather(searchBox.value);
+});
